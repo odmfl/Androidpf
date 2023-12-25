4976bb34b772f5bf2addd9c031121bc8b7c6e654
odmfl <77236251+odmfl@users.noreply.github.com>
2023-12-25 22:14:48 +0100

    Update PdfiumCore.java

--- PdfiumAndroid/src/main/java/com/shockwave/pdfium/PdfiumCore.java
+++ PdfiumAndroid/src/main/java/com/shockwave/pdfium/PdfiumCore.java
@@ -171,7 +171,7 @@ public class PdfiumCore {
 
     public long openText(long pagePtr) {
         synchronized (lock) {
-            return nativeLoadTextPage(long pagePtr);
+            return nativeLoadTextPage(long pagePtr)
         }
     }
 
